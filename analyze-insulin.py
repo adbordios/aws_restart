a8d8939975d8b2e08ef24214c266679eaf644d9b
Arvin Bordios <arvin.bordios@gmail.com>
2023-08-23 06:06:32 +0000

    modify analyzie-insulin.py

--- analyze-insulin.py
+++ analyze-insulin.py
@@ -1,26 +1,24 @@
-insulinFile = open("preproinsulin-seq.txt")
 insulinText = insulinFile.read()
-
 
 removeOrigin=insulinText.replace("ORIGIN", "")
 removeSlash=removeOrigin.replace("//", "")
 removeNewline=removeSlash.replace("\n", "")
 noSpace=removeNewline.replace(" ","")
-noDigits = ''.join([i for i in noSpace if not i.isdigit()])
+cleanText = ''.join([i for i in noSpace if not i.isdigit()])
 
 with open("preproinsulin-seq-clean.txt", "w") as preproinsulin:
-    preproinsulin.write(noDigits)
+    preproinsulin.write(cleanText)
 
 with open("lsinsulin-seq-clean.txt", "w") as lsinsulin:
-    lsinsulin.write(noDigits[0:24])
+    lsinsulin.write(cleanText[0:24])
     
 with open("binsulin-seq-clean.txt", "w") as binsulin:
-    binsulin.write(noDigits[24:54])
+    binsulin.write(cleanTexts[24:54])
     
 with open("cinsulin-seq-clean.txt", "w") as cinsulin:
-    cinsulin.write(noDigits[54:89])
+    cinsulin.write(cleanText[54:89])
     
 with open("ainsulin-seq-clean.txt", "w") as ainsulin:
-    ainsulin.write(noDigits[89:])
+    ainsulin.write(cleanText[89:])
 
 insulinFile.close()
