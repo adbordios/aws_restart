ff04d2da303c4d6458b353c7349e7ea9a49c0b66
Arvin Bordios <arvin.bordios@gmail.com>
2023-08-23 06:09:09 +0000

    modify analyzie-insulin.py

--- analyze-insulin.py
+++ analyze-insulin.py
@@ -1,24 +1,25 @@
+insulinFile = open("preproinsulin-seq.txt")
 insulinText = insulinFile.read()
 
 removeOrigin=insulinText.replace("ORIGIN", "")
 removeSlash=removeOrigin.replace("//", "")
 removeNewline=removeSlash.replace("\n", "")
 noSpace=removeNewline.replace(" ","")
-cleanText = ''.join([i for i in noSpace if not i.isdigit()])
+cleanFile = ''.join([i for i in noSpace if not i.isdigit()])
 
 with open("preproinsulin-seq-clean.txt", "w") as preproinsulin:
-    preproinsulin.write(cleanText)
+    preproinsulin.write(cleanFile)
 
 with open("lsinsulin-seq-clean.txt", "w") as lsinsulin:
-    lsinsulin.write(cleanText[0:24])
+    lsinsulin.write(cleanFile[0:24])
     
 with open("binsulin-seq-clean.txt", "w") as binsulin:
-    binsulin.write(cleanTexts[24:54])
+    binsulin.write(cleanFile[24:54])
     
 with open("cinsulin-seq-clean.txt", "w") as cinsulin:
-    cinsulin.write(cleanText[54:89])
+    cinsulin.write(cleanFile[54:89])
     
 with open("ainsulin-seq-clean.txt", "w") as ainsulin:
-    ainsulin.write(cleanText[89:])
+    ainsulin.write(cleanFile[89:])
 
 insulinFile.close()
